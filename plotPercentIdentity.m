7534244b5bb35ba1d7346d5f1c8caf8b5c72ede2
Eleni Christoforidou <christoforidoueleni@gmail.com>
2020-05-26 18:59:07 +0100

    Add files via upload

--- plotPercentIdentity.m
+++ plotPercentIdentity.m
@@ -18,9 +18,9 @@ function plotPercentIdentity
 %the working directory.
 
 %%
-%Supblot 1.
+%Subplot 1.
 tdfread('NanoPlot-data_barcode07filtered.txt'); %load data.
-clear mapQ lengths aligned_lengths readIDs
+clear mapQ lengths aligned_lengths readIDs aligned_quals quals
 fig=figure('Position', get(0,'Screensize')); %make figure full-screen.
 ax(1)=subplot(2,3,1);
 h1=histogram(percentIdentity,100); %create histogram.
@@ -32,10 +32,11 @@ set(gca,'FontSize',12); %set fonstsize of axis values.
 set(gca,'box','off'); %remove top x-axis and right y-axis.
 h1.FaceColor=[.5 .8 .7]; %change bar fill colour.
 h1.EdgeColor=[.5 .8 .7]; %change bar outline colour.
+ylim([0 450000]);
 
-%Supblot 2.
+%Subplot 2.
 tdfread('NanoPlot-data_barcode08filtered.txt'); %load data.
-clear mapQ lengths aligned_lengths readIDs
+clear mapQ lengths aligned_lengths readIDs aligned_quals quals
 ax(2)=subplot(2,3,2);
 h2=histogram(percentIdentity,100); %create histogram.
 clear percentIdentity;
@@ -46,10 +47,11 @@ set(gca,'FontSize',12); %set fonstsize of axis values.
 set(gca,'box','off'); %remove top x-axis and right y-axis.
 h2.FaceColor=[.3 .6 .5]; %change bar fill colour.
 h2.EdgeColor=[.3 .6 .5]; %change bar outline colour.
+ylim([0 450000]);
 
-%Supblot 3.
+%Subplot 3.
 tdfread('NanoPlot-data_barcode09filtered.txt'); %load data.
-clear mapQ lengths aligned_lengths readIDs
+clear mapQ lengths aligned_lengths readIDs aligned_quals quals
 ax(3)=subplot(2,3,3);
 h3=histogram(percentIdentity,100); %create histogram.
 clear percentIdentity;
@@ -60,10 +62,11 @@ set(gca,'FontSize',12); %set fonstsize of axis values.
 set(gca,'box','off'); %remove top x-axis and right y-axis.
 h3.FaceColor=[.1 .4 .3]; %change bar fill colour.
 h3.EdgeColor=[.1 .4 .3]; %change bar outline colour.
+ylim([0 450000]);
 
-%Supblot 4.
+%Subplot 4.
 tdfread('NanoPlot-data_barcode10filtered.txt'); %load data.
-clear mapQ lengths aligned_lengths readIDs
+clear mapQ lengths aligned_lengths readIDs aligned_quals quals
 ax(4)=subplot(2,3,4);
 h4=histogram(percentIdentity,100); %create histogram.
 clear percentIdentity;
@@ -74,10 +77,11 @@ set(gca,'FontSize',12); %set fonstsize of axis values.
 set(gca,'box','off'); %remove top x-axis and right y-axis.
 h4.FaceColor=[.8 .5 .7]; %change bar fill colour.
 h4.EdgeColor=[.8 .5 .7]; %change bar outline colour.
+ylim([0 450000]);
 
-%Supblot 5.
+%Subplot 5.
 tdfread('NanoPlot-data_barcode11filtered.txt'); %load data.
-clear mapQ lengths aligned_lengths readIDs
+clear mapQ lengths aligned_lengths readIDs aligned_quals quals
 ax(5)=subplot(2,3,5);
 h5=histogram(percentIdentity,100); %create histogram.
 clear percentIdentity;
@@ -88,10 +92,11 @@ set(gca,'FontSize',12); %set fonstsize of axis values.
 set(gca,'box','off'); %remove top x-axis and right y-axis.
 h5.FaceColor=[.6 .3 .5]; %change bar fill colour.
 h5.EdgeColor=[.6 .3 .5]; %change bar outline colour.
+ylim([0 450000]);
 
-%Supblot 6.
+%Subplot 6.
 tdfread('NanoPlot-data_barcode12filtered.txt'); %load data.
-clear mapQ lengths aligned_lengths readIDs
+clear mapQ lengths aligned_lengths readIDs aligned_quals quals
 ax(6)=subplot(2,3,6);
 h6=histogram(percentIdentity,100); %create histogram.
 clear percentIdentity;
@@ -102,6 +107,7 @@ set(gca,'FontSize',12); %set fonstsize of axis values.
 set(gca,'box','off'); %remove top x-axis and right y-axis.
 h6.FaceColor=[.4 .1 .3]; %change bar fill colour.
 h6.EdgeColor=[.4 .1 .3]; %change bar outline colour.
+ylim([0 450000]);
 
 linkaxes(ax,'xy'); %link x-axis and y-axis limits of all subplots.
 
